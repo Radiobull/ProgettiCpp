717c8b0242f8c7b8b1a0497ff736141e537d661f
Radiobull <104358950+Radiobull@users.noreply.github.com>
2022-06-06 15:56:04 +0200

    Add files via upload

--- database/main.cpp
+++ database/main.cpp
@@ -1,39 +1,87 @@
+// librerie
 #include <iostream>
 
 using namespace std;
 
-int main()
-{
-    int a = 0;
-    int b = 0;
-    int c = 0;
-    
-    int x[3][3][3] = {
-      
-      {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
-      {{10, 11, 12}, {13, 14, 15}, {16, 17, 18}},
-      {{19, 20, 21}, {22, 23, 24}, {25, 26, 27}}
-        
-    };
+int main() {
 
-    //cout << x[2][1][1];
-    
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++) {
-            for(int k = 0; k < 3; k++) {
-                cout << x[i][j][k] << "\t";
+    // variabili
+    short azione = 0;
+    short iAzione = 1;
+    short Iprogramma = 1;
+
+    unsigned short x = 0;
+    unsigned short y = 0;
+
+    while(Iprogramma) {
+
+        cout << "benvenuto utente, per prima cosa dovra creare un database.\n\n";
+
+        cout << "inserire la grandezza del database:\n\ngrandezza y: ";
+        cin >> y;
+        cout << "grandrzza x: ";
+        cin >> x;
+        cout << endl;
+
+        float **database = new float*[x]; // variabile che contiene puntatore per righe
+
+        for (int i = 0; i < x; i++) {
+            database[i] = new float[y]; // puntatore colonna che si genera per ogni puntatore riga
+        }
+
+        // lettura del database (da parte del pc)
+        for(int i = 0; i < y; i++) {
+            for(int j = 0; j < x; j++) {
+                cout << "dato del database: ";
+                cin >> database[i][j];
             }
         }
-    }
-    
-    cout << "\nspecificare il numero di righe(si conta a partire da 0)\n";
-    cin >> a;
-    cout << "specificare il numero di colonna\n";
-    cin >> b;
-    cout << "specificare il numero di gruppo composto da 3 es. (1, 2, 3) il numero di gruppo 2 e il 3\n";
-    cin >> c;
-    cout << endl;
-    cout << x[a][b][c];
 
+        cout << endl << "database corrente:\n";
+
+        // scrittura del database (da parte del pc)
+        for(int i = 0; i < y; i++) {
+            for(int j = 0; j < x; j++) {
+                cout << database[i][j] << endl;
+            }
+        }
+
+        iAzione = 1; // sto resettando il valore per rientrare nel ciclo
+
+        while(iAzione) {
+            cout << "\nscegliere l'azione da fare:\n1- riscrivere database\n2- rileggere database\n3- arrestare il programma\n";
+            cin >> azione;
+            cout << endl;
+
+            if (azione == 1) {
+                iAzione = 0; // per uscire dal ciclo
+            }
+
+            else if (azione == 2) {
+
+                cout << "database corrente:\n";
+
+                // scrittura del database (da parte del pc)
+                for(int i = 0; i < y; i++) {
+                    for(int j = 0; j < x; j++) {
+                        cout << database[i][j] << endl;
+                    }
+                }
+            }
+
+            else if (azione == 3) {
+                cout << "il programma si arrestera\n";
+                iAzione = 0; // per uscire dal ciclo
+                Iprogramma = 0; // per arrestare il programma, uscendo dal ciclo iniziale e dando valore 0
+            }
+
+            else {
+                cout << "errore nella digitazione(il programma ripartira)\n\n";
+                iAzione = 0; // per uscire dal ciclo
+            }
+
+        }
+
+    }
     return 0;
 }
