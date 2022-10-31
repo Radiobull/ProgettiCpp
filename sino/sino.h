4b1e4ce82d27321bdde01c8e0daa00d90eaff2e0
Radiobull <104358950+Radiobull@users.noreply.github.com>
2022-10-31 01:14:46 +0100

    Add files via upload

--- sino/sino.h
+++ sino/sino.h
@@ -3,4 +3,20 @@ using namespace std;
 
 #pragma once
 
-short sino(string risp);
+/* questo programma serve per mettere due if per un si/no con output 1, 2 e 3 per manipolare la risposta ("sino" sta per si no)
+1- errore
+2- si
+3- no*/
+
+short sino(string risp) {
+    
+    if (risp == "s" || risp == "S" || risp == "si" || risp == "SI" || risp == "sI") {
+        return 2;
+    }
+    
+    else if (risp == "n" || risp == "N" || risp == "no" || risp == "NO" || risp == "nO") {
+        return 3;
+    }
+    
+    return 1;
+}
